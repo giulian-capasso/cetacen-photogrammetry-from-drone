2cd5324c81e2be139081e3e3cd22ece3d00f0a7d
Giulian Capasso <142629447+giulian-capasso@users.noreply.github.com>
2024-01-15 16:25:47 +0100

    Update functions_15JAN.r

--- functions_15JAN.r
+++ functions_15JAN.r
@@ -19,9 +19,81 @@ spike_cleaning <- function(data, column_name, new_column_name, lower_limit, uppe
   
   return(result)
 }
-
 FLY304_REC_MOT_LD_1.2 <- spike_cleaning(FLY304_REC_MOT_LD_1.2, column_name = "laser_altitude_m", new_column_name = "laser_altitude_m_cleaned", lower_limit = 2, upper_limit = 60, omit_first_n = 5, omit_last_n = 5)
 
+correct_altitude <- function(dataset) {
+  if (!("laser_altitude_m_cleaned" %in% colnames(dataset)) ||
+      !("tilt_corrected" %in% colnames(dataset))) {
+    stop("Le colonne richieste non sono presenti nel dataset.")
+  }
+  
+  dataset$laser_altitude_m_corrected <- ifelse(
+    is.na(dataset$laser_altitude_m_cleaned) | is.na(dataset$tilt_corrected),
+    NA,  # Imposta il valore su NA se uno dei valori è NA
+    round(dataset$laser_altitude_m * cos(dataset$tilt_corrected * pi / 180), 2)
+  )
+  return(dataset)
+}
+FLY304_REC_MOT_LD_1.2 <- correct_altitude(FLY304_REC_MOT_LD_1.2)
+
+GAP3 <- function(data, col1, col2, index_ranges) {
+  # Verifica se le colonne col1 e col2 sono presenti nei dati
+  if (!(col1 %in% names(data)) || !(col2 %in% names(data))) {
+    stop("Le colonne specificate non sono presenti nei dati.")
+  }
+  
+  # Inizializza una colonna per le differenze
+  data$diff_col <- NA
+  
+  # Calcola le differenze per ciascun intervallo specificato
+  for (range in index_ranges) {
+    start_index <- range[1]
+    end_index <- range[2]
+    
+    # Verifica se gli indici sono validi e se ci sono dati nel range specificato
+    if (!is.na(start_index) && !is.na(end_index) &&
+        start_index <= end_index &&
+        start_index >= 1 && end_index <= nrow(data) &&
+        !all(is.na(data[[col1]][start_index:end_index])) &&
+        !all(is.na(data[[col2]][start_index:end_index]))) {
+      
+      # Calcola le differenze nel range specificato
+      differences <- data[[col1]][start_index:end_index] - data[[col2]][start_index:end_index]
+      
+      # Assegna le differenze alla colonna diff_col
+      data$diff_col[start_index:end_index] <- differences
+    } else {
+      warning("Intervallo non valido: ", paste("start_index =", start_index, ", end_index =", end_index))
+    }
+  }
+  
+  # Restituisci il dataset modificato
+  return(data)
+}
+FLY304_REC_MOT_LD_1.2 <- GAP3(FLY304_REC_MOT_LD_1.2, "laser_altitude_m_cleaned", "osd_data:relativeHeight[meters]", list(c(265, 451), c(481, 541), c(571, 591), c(611, 671), c(691, 771),c(861, 911)))
+
+calculate_smoothed_diff <- function(vector) {
+  result <- rep(NA, length(vector))
+  
+  for (i in 3:(length(vector) - 2)) {
+    if (!is.na(vector[i])) {
+      if (is.na(vector[i - 1]) && is.na(vector[i - 2]) && is.na(vector[i - 3])) {
+        result[i] <- vector[i]
+      } else if (!is.na(vector[i - 1]) && is.na(vector[i - 2]) && is.na(vector[i - 3])) {
+        result[i] <- (vector[i - 1] + vector[i]) / 2
+      } else if (is.na(vector[i - 1]) && is.na(vector[i - 2]) && !is.na(vector[i + 1]) && !is.na(vector[i + 2])) {
+        result[i] <- (vector[i + 1] + vector[i + 2]) / 2
+      } else if (!is.na(vector[i - 1]) && !is.na(vector[i - 2]) && is.na(vector[i + 1]) && is.na(vector[i + 2])) {
+        result[i] <- (vector[i - 1] + vector[i - 2]) / 2
+      } else {
+        result[i] <- (vector[i - 1] + vector[i] + vector[i + 1]) / 3
+      }
+    }
+  }
+  
+  return(result)
+}
+FLY304_REC_MOT_LD_1.2$diff_smooth <- calculate_smoothed_diff(FLY304_REC_MOT_LD_1.2$diff_col)
 
 # ----------------------------------------------------------------------------------------------------------------------- #
 #### Applicazione correzione tilt_corrected a laser_altitude_m_cleaned in una nuova colonna laser_altitude_m_corrected ####
